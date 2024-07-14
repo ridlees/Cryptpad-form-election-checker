c1833e63506239f6fec561622f7aff1c4a5dead9
Pták <ptakomuz@proton.me>
2024-07-14 16:33:31 +0200

    Makes main.py executable with arguments

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import json
 import operator
+import sys
 
-election = {}
-with open('Form - Sat, 13 July 2024-4.json') as fp:
-    data = json.load(fp)
-    responses = data["responses"]
-    questions = data["questions"]
-    for question in questions:
-        print(questions[question]["question"])
-        print("\n\n")
-        for response in responses:
-            votes = response[question]
-            vote_weight = 1
-            for vote in votes:
-                if vote == "Abstain":
-                    break
-                if vote in election:
-                    election[vote] = election[vote] + vote_weight
-                else:
-                    election[vote] = vote_weight
-                vote_weight = vote_weight/2
+def get_winner(path):
+    election = {}
+    with open(path) as fp:
+        data = json.load(fp)
+        responses = data["responses"]
+        questions = data["questions"]
+        for question in questions:
+            print(questions[question]["question"])
+            print("\n\n")
+            for response in responses:
+                votes = response[question]
+                vote_weight = 1
+                for vote in votes:
+                    if vote == "Abstain":
+                        break
+                    if vote in election:
+                        election[vote] = election[vote] + vote_weight
+                    else:
+                        election[vote] = vote_weight
+        vote_weight = vote_weight/2
+        return election
                 
-print(max(election.items(), key=operator.itemgetter(1)))
+
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    election = get_winner(path)
+    print(max(election.items(), key=operator.itemgetter(1)))
+    print("\nDone!")
